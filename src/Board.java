64b966102d9aa754dd91415846824c47d22a1b8a
BrownPants21 <s312454@student.salfordcc.ac.uk>
2025-03-27 16:29:09 +0000

    player image

--- src/Board.java
+++ src/Board.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 
+import static utils.Constants.BOARD_HEIGHT;
+import static utils.Constants.BOARD_WIDTH;
+
 public class Board extends JPanel {
     public Board(){
-        setPreferredSize(new Dimension(48, 400));
+        setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT ));
     }
 
 }
---
+++ src/utils/Constants.java
@@ -0,0 +1,12 @@
+package utils;
+
+
+public class Constants {
+    private Constants(){
+
+    }
+    public static final int BOARD_WIDTH = 640;
+    public  static final int BOARD_HEIGHT = 480;
+
+    public static final String PLAyER_IMAGE_PATH = "rescources/amogus.png";
+}
